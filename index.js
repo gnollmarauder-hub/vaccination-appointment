b20dc48cde7895d77cb1dd27e92e603941fe6cf7
dairan <dairan@winning.com.cn>
2020-06-04 17:27:12 +0800

    已经弄到截图了

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const puppeteer = require('puppeteer');
 const config = require('./config.js');
+const fs = require('fs');
 
 (async () => {
+  let timeout = function (delay) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        try {
+          resolve(1)
+        } catch (e) {
+          reject(0)
+        }
+      }, delay);
+    })
+  }
   const browser = await (puppeteer.launch({
     //设置超时时间
     timeout: 15000,
@@ -33,77 +45,97 @@ const config = require('./config.js');
     a = i
   }
   const sliderBtn = await page.$('.gt_slider_knob')
-  sliderBtn.click()
-  const captchaDom = await page.$eval('.gt_cut_fullbg_slice', function (el) {
-    return el.getAttribute('style')
-  })
-  await page.evaluate(captchaDom => {
+  const abc = await page.$('.gt_cut_fullbg')
+  sliderBtn.click({ delay: 3000 })
+  await timeout(2000)
+  abc.screenshot({ path: 'fullBg.png', type: 'png' })
+  await timeout(2000)
+  sliderBtn.click({ delay: 1000 })
+  abc.screenshot({ path: 'gapBg.png', type: 'png' })
+  const fullBgBuff = Buffer.from(fs.readFileSync('./fullBg.png'), 'base64')
+  const gapBgBuff = Buffer.from(fs.readFileSync('./gapBg.png'), 'base64')
 
+  await page.evaluate(({ fullBgBuff, gapBgBuff }) => {
+    console.log(fullBgBuff)
     let fullBgDom = document.createElement('img')
-    let canvasDom = document.createElement('canvas')
-    fullBgDom.setAttribute('id', 'img')
-    //width="200" height="200"
-    //background-image: url("http://static.geetest.com/pictures/gt/1c255d166/1c255d166.webp"); background-position: -157px -80px;
-    fullBgDom.setAttribute('src', captchaDom.match(/http:\/\/static.geetest.com\/pictures\/gt\/[a-zA-z0-9]+\/[a-zA-z0-9]+.webp/))
-    canvasDom.setAttribute('id', 'imageC')
+    fullBgDom.setAttribute('src', fullBgBuff)
+    fullBgDom.setAttribute('id', 'fullBg')
+    let gapBgDom = document.createElement('img')
+    gapBgDom.setAttribute('src', gapBgBuff)
+    gapBgDom.setAttribute('id', 'gapBg')
     document.body.appendChild(fullBgDom)
-    document.body.appendChild(canvasDom)
-    var canvas = document.getElementById('imageC')
-    fullBgDom = document.getElementById('img')
-    fullBgDom.onload = function (options) {
-      console.log(options, 'options')
-      var ctx = canvas.getContext('2d')
-      ctx.drawImage(fullBgDom, 0, 0, 260, 180)
-      setTimeout(() => {
-        clickCropImage()
-      }, 3000)
-    }
-    function clickCropImage () {
-      const numberX = 26
-      const numberY = 2
-      const splaceX = 10
-      const splaceY = 80
-      var canvas = document.getElementById('imageC');
-      //  获取画布大小，判断画布大小
-      var canvasH = canvas.clientHeight;
-      var canvasW = canvas.clientWidth;
-      //  将图片等分
-      for (var x = 0; x < numberX; x++) {
-        for (var y = 0; y < numberY; y++) {
-          var location = {
-            'x': x * canvasW / numberX - splaceX,
-            'y': y * canvasH / numberY - splaceY,
-            'Dx': x * canvasW / numberX + canvasW / numberX,
-            'Dy': y * canvasH / numberY + canvasH / numberY,
-            'locationOption': (x + 1).toString() + (y + 1).toString(),
-          };
-          // locationArr.push(location);
-          cropImage(canvas, (x * canvasW / numberX) - splaceX, (y * canvasH / numberY) - splaceY, canvasW / numberX, canvasH / numberY);
-        };
-      };
-      canvasComimgCreated = false;
-      divComimgCreated = false;
-    }
+    document.body.appendChild(gapBgDom)
+  }, { fullBgBuff, gapBgBuff })
+  // const captchaDom = await page.$eval('.gt_cut_fullbg_slice', function (el) {
+  //   return el.getAttribute('style')
+  // })
+  // await page.evaluate(captchaDom => {
 
-    function cropImage (targetCanvas, x, y, width, height) {
-      var targetctx = targetCanvas.getContext('2d');
-      var targetctxImageData = targetctx.getImageData(x, y, width, height);
-      // sx, sy, sWidth, sHeight
-      var c = document.createElement('canvas');
-      var ctx = c.getContext('2d');
-      c.width = width;
-      c.height = height;
-      ctx.rect(0, 0, width, height);
-      ctx.fillStyle = 'white';
-      ctx.fill();
-      ctx.putImageData(targetctxImageData, 0, 0);
-      // imageData, dx, dy
-      // 创建img 块
-      var img = document.createElement('img');
-      img.src = c.toDataURL('image/jpeg', 0.92);
-      document.body.appendChild(img);
-    }
-  }, captchaDom)
+  //   let fullBgDom = document.createElement('img')
+  //   let canvasDom = document.createElement('canvas')
+  //   fullBgDom.setAttribute('id', 'img')
+  //   //width="200" height="200"
+  //   //background-image: url("http://static.geetest.com/pictures/gt/1c255d166/1c255d166.webp"); background-position: -157px -80px;
+  //   fullBgDom.setAttribute('src', captchaDom.match(/http:\/\/static.geetest.com\/pictures\/gt\/[a-zA-z0-9]+\/[a-zA-z0-9]+.webp/))
+  //   canvasDom.setAttribute('id', 'imageC')
+  //   document.body.appendChild(fullBgDom)
+  //   document.body.appendChild(canvasDom)
+  //   var canvas = document.getElementById('imageC')
+  //   fullBgDom = document.getElementById('img')
+  //   fullBgDom.onload = function (options) {
+  //     console.log(options, 'options')
+  //     var ctx = canvas.getContext('2d')
+  //     ctx.drawImage(fullBgDom, 0, 0, 260, 180)
+  //     setTimeout(() => {
+  //       clickCropImage()
+  //     }, 3000)
+  //   }
+  //   function clickCropImage () {
+  //     const numberX = 26
+  //     const numberY = 2
+  //     const splaceX = 10
+  //     const splaceY = 80
+  //     var canvas = document.getElementById('imageC');
+  //     //  获取画布大小，判断画布大小
+  //     var canvasH = canvas.clientHeight;
+  //     var canvasW = canvas.clientWidth;
+  //     //  将图片等分
+  //     for (var x = 0; x < numberX; x++) {
+  //       for (var y = 0; y < numberY; y++) {
+  //         var location = {
+  //           'x': x * canvasW / numberX - splaceX,
+  //           'y': y * canvasH / numberY - splaceY,
+  //           'Dx': x * canvasW / numberX + canvasW / numberX,
+  //           'Dy': y * canvasH / numberY + canvasH / numberY,
+  //           'locationOption': (x + 1).toString() + (y + 1).toString(),
+  //         };
+  //         // locationArr.push(location);
+  //         cropImage(canvas, (x * canvasW / numberX) - splaceX, (y * canvasH / numberY) - splaceY, canvasW / numberX, canvasH / numberY);
+  //       };
+  //     };
+  //     canvasComimgCreated = false;
+  //     divComimgCreated = false;
+  //   }
+
+  //   function cropImage (targetCanvas, x, y, width, height) {
+  //     var targetctx = targetCanvas.getContext('2d');
+  //     var targetctxImageData = targetctx.getImageData(x, y, width, height);
+  //     // sx, sy, sWidth, sHeight
+  //     var c = document.createElement('canvas');
+  //     var ctx = c.getContext('2d');
+  //     c.width = width;
+  //     c.height = height;
+  //     ctx.rect(0, 0, width, height);
+  //     ctx.fillStyle = 'white';
+  //     ctx.fill();
+  //     ctx.putImageData(targetctxImageData, 0, 0);
+  //     // imageData, dx, dy
+  //     // 创建img 块
+  //     var img = document.createElement('img');
+  //     img.src = c.toDataURL('image/jpeg', 0.92);
+  //     document.body.appendChild(img);
+  //   }
+  // }, captchaDom)
   // 登陆
   // 预约
   // await page.goto('http://www.ah12320.com/jsp/vaccination/scheduleInfo.jsp?shopId=3401231301');
@@ -124,4 +156,14 @@ const config = require('./config.js');
   // browser.close();
 })();
 
+/**
+ * 计算滑块位置
+*/
+async function getBtnPosition (id, page) {
+  const btn_position = await page.evaluate((id) => {
+    const { clientWidth, clientHeight } = document.querySelector(id)
+    return { btn_left: clientWidth / 2 - 22, btn_top: clientHeight / 2 + 22 }
+  }, id)
+  return btn_position;
+}
 
